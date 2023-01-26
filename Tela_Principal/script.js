ec920e58075fd5574ce772e3bb887b1754c06edd
JardelLeoncio <121761534+JardelLeoncio@users.noreply.github.com>
2023-01-26 01:40:34 -0300

    att

---
+++ Tela_Principal/script.js
@@ -0,0 +1,14 @@
+function menuShow()
+{
+    let menuMobile = document.querySelector('.mobile-menu');
+    if(menuMobile.classList.contains('open'))
+    {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "Tela_Principal/imagens/menu-white.svg";
+    }
+    else
+    {
+        menuMobile.classList.add('open')
+        document.querySelector('.icon').src = "Tela_Principal/imagens/close-white.svg";
+    }
+}
