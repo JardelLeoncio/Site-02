f3ea09d8b707ca38a7f3e4afed5cb750e890ba7a
JardelLeoncio <121761534+JardelLeoncio@users.noreply.github.com>
2023-01-26 04:38:25 -0300

    att

--- Tela_Principal/script.js
+++ Tela_Principal/script.js
@@ -8,7 +8,16 @@ function menuShow()
     }
     else
     {
-        menuMobile.classList.add('open')
+        menuMobile.classList.add('open');
         document.querySelector('.icon').src = "Tela_Principal/imagens/close-white.svg";
+    }
+}
+function closeShow()
+{
+    let menuMobile = document.querySelector('.mobile-menu');
+    if(menuMobile.classList.contains('open'))
+    {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "Tela_Principal/imagens/menu-white.svg";
     }
 }
