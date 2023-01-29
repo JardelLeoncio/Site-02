899ec4ecff7ebeb5621b93254695c9c110b1bc67
JardelLeoncio <121761534+JardelLeoncio@users.noreply.github.com>
2023-01-29 20:57:59 -0300

    att

--- Tela_Principal/script_index.js
+++ Tela_Principal/script_index.js
@@ -21,3 +21,34 @@ function closeShow()
         document.querySelector('.icon').src = "Tela_Principal/imagens/menu-white.svg";
     }
 }
+function cadastroShow()
+{
+    let trocacadastro = document.querySelector('.cadastro');
+    let trocalogin = document.querySelector('.login');
+    if(trocacadastro.classList.contains('troca-cadastro'))
+    {
+        trocacadastro.classList.remove('troca-cadastro');
+        trocalogin.classList.remove('troca-login');
+    }
+    else
+    {
+        trocacadastro.classList.add('troca-cadastro');
+        trocalogin.classList.add('troca-login');
+    }
+}
+function loginShow()
+{
+    let trocalogin = document.querySelector('.login');
+    let trocacadastro = document.querySelector('.cadastro');
+    if(trocalogin.classList.contains('troca-login'))
+    {
+        trocalogin.classList.remove('troca-login');
+        trocacadastro.classList.remove('troca-cadastro');
+        
+    }
+    else
+    {
+        trocalogin.classList.add('troca-login');
+        trocacadastro.classList.add('troca-cadastro');
+    }
+}
