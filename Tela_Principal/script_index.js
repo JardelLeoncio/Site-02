1c85c6befaa56d4ca8672c1abb77430a1359ebea
JardelLeoncio <121761534+JardelLeoncio@users.noreply.github.com>
2023-01-30 09:42:13 -0300

    att

--- Tela_Principal/script_index.js
+++ Tela_Principal/script_index.js
@@ -1,37 +1,29 @@
-function menuShow()
-{
+function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
-    if(menuMobile.classList.contains('open'))
-    {
+    if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
         document.querySelector('.icon').src = "Tela_Principal/imagens/menu-white.svg";
     }
-    else
-    {
+    else {
         menuMobile.classList.add('open');
         document.querySelector('.icon').src = "Tela_Principal/imagens/close-white.svg";
     }
 }
-function closeShow()
-{
+function closeShow() {
     let menuMobile = document.querySelector('.mobile-menu');
-    if(menuMobile.classList.contains('open'))
-    {
+    if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
         document.querySelector('.icon').src = "Tela_Principal/imagens/menu-white.svg";
     }
 }
-function trocaShow()
-{
+function trocaShow() {
     let trocacadastro = document.querySelector('.cadastro');
     let trocalogin = document.querySelector('.login');
-    if(trocacadastro.classList.contains('troca-cadastro'))
-    {
+    if (trocacadastro.classList.contains('troca-cadastro')) {
         trocacadastro.classList.remove('troca-cadastro');
         trocalogin.classList.remove('troca-login');
     }
-    else
-    {
+    else {
         trocacadastro.classList.add('troca-cadastro');
         trocalogin.classList.add('troca-login');
     }
